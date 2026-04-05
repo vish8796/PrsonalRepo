8e283ebbd4edc7fd168bfd8fede7767e0e8315b7
Vishwajeet <globantimt03@gmail.com>
2026-04-05 20:57:40 +0530

    second

---
+++ myPractice/src/practiceCodes/Div.java
@@ -0,0 +1,11 @@
+package practiceCodes;
+
+// Not using object and directly performing action under main method
+
+public class Div {
+
+	public static void main(String[] args) {
+		int Div = 50 / 3;
+		System.out.println(Div);
+	}
+}
---
+++ myPractice/src/practiceCodes/Hello.java
@@ -0,0 +1,12 @@
+package practiceCodes;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Vishwajeet Loni");
+		
+
+	}
+
+}
